b732b58b3112453be25d1c2697adbf74b8158abe
xfactor4 <gdrobinson92@comcast.net>
2015-10-13 08:59:14 -0400

    Update 2015-10-13T12:59:12.614Z

--- app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import MenuRouter from 'router';
-
-$(document).ready(function(){
-
-  window.menuRouter = new MenuRouter();
-  Backbone.history.start();
-
-});
--- app/scripts/models/menu-collection.js
+++
@@ -1,8 +0,0 @@
-import Menu from 'models/menu';
-
-var MenuCollection = Backbone.Collection.extend({
-  model: Menu,
-  url: 'http://tiny-lasagna-server.herokuapp.com/collections/GRthai?limit=100'
-});
-
-export default MenuCollection;
